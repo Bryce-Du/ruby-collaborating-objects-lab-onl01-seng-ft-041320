c6baa71375138887758dd47789c174aa774b8e8c
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-27 19:32:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,6 +14,12 @@ class Song
   
   def self.new_by_filename(filename)
     data = filename.split(" - ")
+    song = self.new(data[1])
+    song.artist = 
+  end
+  
+  def artist_name=(name)
+    Artist.find_or_create_by_name(name)
     
   end
 end
