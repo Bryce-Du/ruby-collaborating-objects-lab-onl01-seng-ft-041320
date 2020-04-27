b5e4f8731fd1e1f0e21919a6bdafa89fc6764708
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-27 19:41:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,12 +14,12 @@ class Song
   
   def self.new_by_filename(filename)
     data = filename.split(" - ")
-    song = self.new(data[1])
-    song.artist = 
+    new_song = self.new(data[1])
+    new_song.artist_name = data[0] 
   end
   
   def artist_name=(name)
     @artist = Artist.find_or_create_by_name(name)
-    
+    @artist.add_song(self)
   end
 end
