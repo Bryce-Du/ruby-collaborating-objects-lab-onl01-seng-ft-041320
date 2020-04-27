211fe12ce9ade5066c04479d9de72469754bc28a
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-27 21:05:08 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,12 +1,15 @@
 class MP3Importer
-  attr_accessor :path 
+  attr_accessor :path, :mp3s 
   
   def initialize (file_path)
     @path = file_path
   end
   def files
-    mp3s = Dir.entries(@path).select{|file| file.include?(".mp3")}
+    @mp3s = Dir.entries(@path).select{|file| file.include?(".mp3")}
   end
   def import 
+    @mp3s.each do |mp3|
+      Song.new_by_filename(mp3)
+    end
   end
 end
